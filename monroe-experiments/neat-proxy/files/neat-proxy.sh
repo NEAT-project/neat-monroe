520a65866d3c90dddea6d9fc08e6012d269b4884
Tomasz Rozensztrauch <t.rozensztrauch@radytek.com>
2018-03-11 15:58:30 +0100

    monroe-experiments: Added socat proxy to dlb for metadata-exporter

--- monroe-experiments/neat-proxy/files/neat-proxy.sh
+++ monroe-experiments/neat-proxy/files/neat-proxy.sh
@@ -3,6 +3,7 @@
 DATE=`date +%Y%m%d-%H%M%S`
 
 # Start policy manager
+
 mkdir -p /var/run/neat/cib/
 mkdir -p /var/run/neat/pib/
 python3.5 /opt/celerway/neat/policy/neatpmd \
@@ -10,15 +11,21 @@ python3.5 /opt/celerway/neat/policy/neatpmd \
   --cib /var/run/neat/cib/ \
   --pib /var/run/neat/pib/ \
   > /monroe/results/neatpmd-${DATE}.txt 2>&1 &
+
+# start socat proxy to dlb
+
+socat TCP-LISTEN:88,fork,bind=172.17.0.1 TCP:127.0.0.1:88 &
  
 # Start neat metadata exporter
+
 neat-metadata-exporter --cib-socket /var/run/neat/neat_cib_socket \
+  --ifname-key InterfaceName \
   > /monroe/results/metadata-exporter-${DATE}.txt 2>&1 &
 
 # Start the proxy
+
 export NEAT_PM_SOCKET="/var/run/neat/neat_pm_socket"
 export NEAT_CIB_SOCKET="/var/run/neat/neat_cib_socket"
-
 neat-proxy > /monroe/results/neat-proxy-${DATE}.txt 2>&1
 
 while true; do

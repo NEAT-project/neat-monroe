e41d3ce5f06346f09e7883646ee18cc10aff8866
Tomasz Rozensztrauch <t.rozensztrauch@radytek.com>
2018-03-12 19:04:21 +0100

    monroe-experiments: Improved neat-benefits result output

--- monroe-experiments/neat-benefits/files/neat-benefits.sh
+++ monroe-experiments/neat-benefits/files/neat-benefits.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DATE=`date +%Y%m%d-%H%M%S`
+RESULT_DIR="/monroe/results/"
 
 # Apply policies and start policy manager
 
@@ -24,10 +25,11 @@ neat-metadata-exporter --cib-socket /var/run/neat/neat_cib_socket \
 # Run experiment
 
 export NEAT_PM_SOCKET="/var/run/neat/neat_pm_socket"
-export NEAT_CIB_SOCKET="/var/run/neat/neat_cib_socket"
+# export NEAT_CIB_SOCKET="/var/run/neat/neat_cib_socket" (Commented out to disable CIB caching) 
 
 RUN_COUNT=`cat /monroe/config | jq .run_count`
 RUN_INTERVAL=`cat /monroe/config | jq .run_interval`
+RUN_INITIAL_WAIT=`cat /monroe/config | jq .run_initial_wait`
 
 RUN_TCP_PING=`cat /monroe/config | jq -r .run_tcp_ping`
 RUN_NEAT_TCP_PING=`cat /monroe/config | jq -r .run_neat_tcp_ping`
@@ -36,6 +38,8 @@ TCP_PING_PORT=`cat /monroe/config | jq -r .ping_port`
 TCP_PING_MODE=`cat /monroe/config | jq -r .ping_mode`
 TCP_PING_COUNT=`cat /monroe/config | jq -r .ping_count`
 TCP_PING_INTERVAL=`cat /monroe/config | jq -r .ping_interval`
+TCP_PING_BIND_IFNAME=`cat /monroe/config | jq -r .ping_bind_ifname`
+TCP_PING_VERBOSE=`cat /monroe/config | jq -r .ping_verbose`
 
 RUN_DWNL_TEST=`cat /monroe/config | jq -r .run_dwnl_test`
 RUN_NEAT_DWNL_TEST=`cat /monroe/config | jq -r .run_neat_dwnl_test`
@@ -44,67 +48,105 @@ DWNL_TEST_PATH=`cat /monroe/config | jq -r .dwnl_test_path`
 DWNL_TEST_COUNT=`cat /monroe/config | jq -r .dwnl_test_count`
 DWNL_TEST_INTERVAL=`cat /monroe/config | jq -r .dwnl_test_interval`
 DWNL_TEST_SERVER=`cat /monroe/config | jq -r .dwnl_test_server`
+DWNL_TEST_BIND_IFNAME=`cat /monroe/config | jq -r .dwnl_test_bind_ifname`
+DWNL_TEST_VERBOSE=`cat /monroe/config | jq -r .dwnl_test_verbose`
+
+
+# Sleep for a while to let metadata-exporter produce some CIBs
+sleep ${RUN_INITIAL_WAIT}
 
 COUNT=0
 while [ $COUNT -lt ${RUN_COUNT} ]; do
 
   # tcp-ping
   if [ "${RUN_TCP_PING}" == "true" ]; then
+    sleep ${RUN_INTERVAL}
+    APP_NAME="tcp-ping"
     DATE=`date +%Y%m%d-%H%M%S`
-    FNAME="/monroe/results/tcp-ping-${DATE}.txt"
-    TMP_FNAME="/tmp/tcp-ping-${DATE}.txt"
+    FNAME_STDOUT="${RESULT_DIR}${APP_NAME}-${DATE}.txt"
+    FNAME_STDERR="${RESULT_DIR}${APP_NAME}-stderr-${DATE}.txt"
+    TMP_FNAME_STDOUT="${TMP_DIR}${APP_NAME}-${DATE}.txt"
+    TMP_FNAME_STDERR="${TMP_DIR}${APP_NAME}-stderr-${DATE}.txt"
     tcp-ping --port=${TCP_PING_PORT} \
       --mode=${TCP_PING_MODE} \
       --count=${TCP_PING_COUNT} \
       --interval=${TCP_PING_INTERVAL} \
-      ${TCP_PING_SERVER} &> ${TMP_FNAME}
-    mv ${TMP_FNAME} ${FNAME}
+      --bind=${TCP_PING_BIND_IFNAME} \
+      --verbose=${TCP_PING_VERBOSE} \
+      ${TCP_PING_SERVER} \
+      1>"${TMP_FNAME_STDOUT}" \
+      2>"${TMP_FNAME_STDERR}"
+    mv ${TMP_FNAME_STDOUT} ${FNAME_STDOUT}
+    mv ${TMP_FNAME_STDERR} ${FNAME_STDERR}
   fi
 
   # dwnl-test
   if [ "${RUN_DWNL_TEST}" == "true" ]; then
+    sleep ${RUN_INTERVAL}
+    APP_NAME="dwnl-test"
     DATE=`date +%Y%m%d-%H%M%S`
-    FNAME="/monroe/results/dwnl-test-${DATE}.txt"
-    TMP_FNAME="/tmp/dwnl-test-${DATE}.txt"
+    FNAME_STDOUT="${RESULT_DIR}${APP_NAME}-${DATE}.txt"
+    FNAME_STDERR="${RESULT_DIR}${APP_NAME}-stderr-${DATE}.txt"
+    TMP_FNAME_STDOUT="${TMP_DIR}${APP_NAME}-${DATE}.txt"
+    TMP_FNAME_STDERR="${TMP_DIR}${APP_NAME}-stderr-${DATE}.txt"
     dwnl-test --port=${DWNL_TEST_PORT} \
       --path=${DWNL_TEST_PATH} \
-       --count=${DWNL_TEST_COUNT} \
-       --interval=${DWNL_TEST_INTERVAL} \
-      ${DWNL_TEST_SERVER} &> ${TMP_FNAME}
-    mv ${TMP_FNAME} ${FNAME}
+      --count=${DWNL_TEST_COUNT} \
+      --interval=${DWNL_TEST_INTERVAL} \
+      --bind=${DWNL_TEST_BIND_IFNAME} \
+      --verbose=${DWNL_TEST_VERBOSE} \
+      ${DWNL_TEST_SERVER} \
+      1>"${TMP_FNAME_STDOUT}" \
+      2>"${TMP_FNAME_STDERR}"
+    mv ${TMP_FNAME_STDOUT} ${FNAME_STDOUT}
+    mv ${TMP_FNAME_STDERR} ${FNAME_STDERR}
   fi
 
   # neat-tcp-ping
   if [ "${RUN_NEAT_TCP_PING}" == "true" ]; then
+    sleep ${RUN_INTERVAL}
+    APP_NAME="neat-tcp-ping"
     DATE=`date +%Y%m%d-%H%M%S`
-    FNAME="/monroe/results/neat-tcp-ping-${DATE}.txt"
-    TMP_FNAME="/tmp/neat-tcp-ping-${DATE}.txt"
+    FNAME_STDOUT="${RESULT_DIR}${APP_NAME}-${DATE}.txt"
+    FNAME_STDERR="${RESULT_DIR}${APP_NAME}-stderr-${DATE}.txt"
+    TMP_FNAME_STDOUT="${TMP_DIR}${APP_NAME}-${DATE}.txt"
+    TMP_FNAME_STDERR="${TMP_DIR}${APP_NAME}-stderr-${DATE}.txt"
     neat-tcp-ping --port=${TCP_PING_PORT} \
       --mode=${TCP_PING_MODE} \
       --count=${TCP_PING_COUNT} \
       --interval=${TCP_PING_INTERVAL} \
-      ${TCP_PING_SERVER} &> ${TMP_FNAME}
-    mv ${TMP_FNAME} ${FNAME}
+      --verbose=${TCP_PING_VERBOSE} \
+      ${TCP_PING_SERVER} \
+      1>"${TMP_FNAME_STDOUT}" \
+      2>"${TMP_FNAME_STDERR}"
+    mv ${TMP_FNAME_STDOUT} ${FNAME_STDOUT}
+    mv ${TMP_FNAME_STDERR} ${FNAME_STDERR}
   fi
 
   # neat-dwnl-test
   if [ "${RUN_NEAT_DWNL_TEST}" == "true" ]; then
+    sleep ${RUN_INTERVAL}
+    APP_NAME="neat-dwnl-test"
     DATE=`date +%Y%m%d-%H%M%S`
-    FNAME="/monroe/results/neat-dwnl-test-${DATE}.txt"
-    TMP_FNAME="/tmp/neat-dwnl-test-${DATE}.txt"
+    FNAME_STDOUT="${RESULT_DIR}${APP_NAME}-${DATE}.txt"
+    FNAME_STDERR="${RESULT_DIR}${APP_NAME}-stderr-${DATE}.txt"
+    TMP_FNAME_STDOUT="${TMP_DIR}${APP_NAME}-${DATE}.txt"
+    TMP_FNAME_STDERR="${TMP_DIR}${APP_NAME}-stderr-${DATE}.txt"
     neat-dwnl-test --port=${DWNL_TEST_PORT} \
       --path=${DWNL_TEST_PATH} \
-       --count=${DWNL_TEST_COUNT} \
-       --interval=${DWNL_TEST_INTERVAL} \
-      ${DWNL_TEST_SERVER} &> ${TMP_FNAME}
-    mv ${TMP_FNAME} ${FNAME}
+      --count=${DWNL_TEST_COUNT} \
+      --interval=${DWNL_TEST_INTERVAL} \
+      --verbose=${DWNL_TEST_VERBOSE} \
+      ${DWNL_TEST_SERVER} \
+      1>"${TMP_FNAME_STDOUT}" \
+      2>"${TMP_FNAME_STDERR}"
+    mv ${TMP_FNAME_STDOUT} ${FNAME_STDOUT}
+    mv ${TMP_FNAME_STDERR} ${FNAME_STDERR}
   fi
 
-  sleep ${RUN_INTERVAL}
   let COUNT=COUNT+1
 done
 
 while true; do
     sleep 15
 done
-
